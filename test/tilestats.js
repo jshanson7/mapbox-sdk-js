b87b3ea8e8f13fdc0fd2beefa8085665a7d1cacc
David Clark <dclark@mapbox.com>
2016-07-20 11:48:55 -0700

    Modify tilestats service to fit new API

--- test/tilestats.js
+++ test/tilestats.js
@@ -4,8 +4,96 @@
 var test = require('tap').test;
 var MapboxClient = require('../lib/services/tilestats');
 var hat = require('../vendor/hat');
+var xtend = require('../vendor/xtend');
+
+var sampleStats = require('./fixtures/tilestats');
+var modifiedStats = xtend(sampleStats, { layerCount: 12 });
 
 test('TilestatsClient', function(tilestatsClient) {
+  var tilesetid = hat().slice(0, 6);
+
+  tilestatsClient.test('#putTilestats', function(putTilestats) {
+    putTilestats.test('typecheck', function(assert) {
+      var client = new MapboxClient(process.env.MapboxAccessToken);
+      assert.ok(client, 'created tilestats client');
+      assert.throws(function() {
+        client.putTilestats(null, {}, function() {});
+      }, 'tileset must be a string');
+      assert.end();
+    });
+
+    putTilestats.test('creates', function(assert) {
+      var client = new MapboxClient(process.env.MapboxAccessToken);
+      client.putTilestats(tilesetid, sampleStats, function(err, result) {
+        assert.ifError(err, 'success');
+        assert.deepEqual(result, xtend(sampleStats, {
+          result: client.owner,
+          tilesetid: tilesetid
+        }), 'expected result');
+        assert.end();
+      });
+    });
+
+    putTilestats.test('updates', function(assert) {
+      var client = new MapboxClient(process.env.MapboxAccessToken);
+      client.putTilestats(tilesetid, modifiedStats, function(err, result) {
+        assert.ifError(err, 'success');
+        assert.deepEqual(result, xtend(modifiedStats, {
+          result: client.owner,
+          tilesetid: tilesetid
+        }), 'expected result');
+        assert.end();
+      });
+    });
+
+    putTilestats.end();
+  });
+
+  tilestatsClient.test('#getTilestats', function(getTilestats) {
+    getTilestats.test('typecheck', function(assert) {
+      var client = new MapboxClient(process.env.MapboxAccessToken);
+      assert.ok(client, 'created dataset client');
+      assert.throws(function() {
+        client.getTilestats(null, function() {});
+      }, 'tileset must be a string');
+      assert.end();
+    });
+
+    getTilestats.test('get tilestats', function(assert) {
+      var client = new MapboxClient(process.env.MapboxAccessToken);
+      client.getTilestats(tilesetid, function(err, result) {
+        assert.ifError(err, 'success');
+        assert.deepEqual(result, xtend(modifiedStats, {
+          account: client.owner,
+          tilesetid: tilesetid
+        }), 'expected result');
+        assert.end();
+      });
+    });
+
+    getTilestats.test('get tilestats when user specified by tilesetid', function(assert) {
+      var client = new MapboxClient(process.env.MapboxAccessToken);
+      var id = client.owner + '.' + tilesetid;
+      client.getTilestats(id, function(err, result) {
+        assert.ifError(err, 'success');
+        assert.deepEqual(result, xtend(modifiedStats, {
+          account: id,
+          tilesetid: tilesetid
+        }), 'expected result');
+        assert.end();
+      });
+    });
+
+    getTilestats.end();
+  });
+
+  tilestatsClient.end();
+});
+
+// x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-
+// LEGACY BELOW: Delete ASAP
+
+test('TilestatsClient legacy', function(tilestatsClient) {
   var tilesetid = hat().slice(0, 6);
 
   tilestatsClient.test('#createTilestats', function(createTilestats) {
@@ -262,78 +350,6 @@ test('TilestatsClient', function(tilestatsClient) {
     });
 
     getTilestatsAttribute.end();
-  });
-
-  tilestatsClient.test('#getTilestats', function(getTilestats) {
-    getTilestats.test('typecheck', function(assert) {
-      var client = new MapboxClient(process.env.MapboxAccessToken);
-      assert.ok(client, 'created dataset client');
-      assert.throws(function() {
-        client.getTilestats(null, function() {});
-      }, 'tileset must be a string');
-      assert.end();
-    });
-
-    getTilestats.test('get tilestats', function(assert) {
-      var client = new MapboxClient(process.env.MapboxAccessToken);
-      client.getTilestats(tilesetid, function(err, result) {
-        assert.ifError(err, 'success');
-        assert.deepEqual(result, {
-          account: client.owner,
-          tilesetid: tilesetid,
-          layers: [
-            {
-              account: client.owner,
-              tilesetid: tilesetid,
-              layer: 'layer',
-              geometry: 'LineString',
-              count: 110,
-              attributes: [
-                {
-                  attribute: 'attr',
-                  min: 2,
-                  max: 2,
-                  values: [2]
-                }
-              ]
-            }
-          ]
-        }, 'expected result');
-        assert.end();
-      });
-    });
-
-    getTilestats.test('get tilestats when user specified by tilesetid', function(assert) {
-      var client = new MapboxClient(process.env.MapboxAccessToken);
-      var id = client.owner + '.' + tilesetid;
-      client.getTilestats(id, function(err, result) {
-        assert.ifError(err, 'success');
-        assert.deepEqual(result, {
-          account: client.owner,
-          tilesetid: id,
-          layers: [
-            {
-              account: client.owner,
-              tilesetid: id,
-              layer: 'layer',
-              geometry: 'LineString',
-              count: 110,
-              attributes: [
-                {
-                  attribute: 'attr',
-                  min: 2,
-                  max: 2,
-                  values: [2]
-                }
-              ]
-            }
-          ]
-        }, 'expected result');
-        assert.end();
-      });
-    });
-
-    getTilestats.end();
   });
 
   tilestatsClient.test('#deleteTilestats', function(deleteTilestats) {
